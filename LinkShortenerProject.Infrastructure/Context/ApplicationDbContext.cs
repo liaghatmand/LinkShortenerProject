1ea8e3be941658d0caee893b709dc60e5c5d6c9c
zahra liaghatmand <zahra.liaghatmand@gmail.com>
2024-08-09 16:10:19 +0330

    Add DbContext
    This is #6 Add DbContext in Infrastructure and IDbContext in Application layer. Configure DbContext overriding OnConfiguring method

---
+++ LinkShortenerProject.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using LinkShortenerProject.Application.Common.Interfaces;
+using LinkShortenerProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkShortenerProject.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        private const string ConnectionString = @"";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+        public DbSet<Link> Links { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
