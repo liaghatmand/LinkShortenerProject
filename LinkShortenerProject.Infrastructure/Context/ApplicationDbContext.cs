8b320e0ac65b3119e522422afb162a157c7d193b
zahra liaghatmand <zahra.liaghatmand@gmail.com>
2024-08-09 16:25:23 +0330

    Change Source of Config for DbContext
    this is #6 Use Dependency Injection to get DbContext Configuration

--- LinkShortenerProject.Infrastructure/Context/ApplicationDbContext.cs
+++ LinkShortenerProject.Infrastructure/Context/ApplicationDbContext.cs
@@ -12,11 +12,6 @@ namespace LinkShortenerProject.Infrastructure.Context
 {
     public class ApplicationDbContext : DbContext, IApplicationDbContext
     {
-        private const string ConnectionString = @"";
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(ConnectionString);
-        }
         public DbSet<Link> Links { get; set; }
         public DbSet<User> Users { get; set; }
     }
